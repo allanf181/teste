1df776eaeee4016f0f1a66b67ae7e0c0d13af057
NAYLOR GARCIA BACHIEGA <naylorgarcia@ifsp.edu.br>
2015-02-26 13:53:53 -0300

    26/02 VARIOS

--- db2/db2Professores.php
+++ db2/db2Professores.php
@@ -42,6 +42,8 @@ for ($n = 1; $n <= 2; $n++) {
         $result = mysql_query($sql);
         $professor = mysql_fetch_object($result);
 
+        print_r($professor);
+        
         print $row->PR_PRONTP ."-".$professor->codigo ."\n";
         //FORMATANDO DADOS
         $sexo = trim($row->PR_SEXO);
