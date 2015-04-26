a4ce85213355169f9ba72462e379be3c77e1ec37
NAYLOR GARCIA BACHIEGA <naylorgarcia@ifsp.edu.br>
2015-04-26 00:01:24 -0300

    26/03 AULA

--- controller/planoAula.class.php
+++ controller/planoAula.class.php
@@ -21,25 +21,53 @@ class PlanosAula extends Generic {
 		AND a.disciplina = d.codigo
                 AND t.codigo = a.turma
                 AND c.codigo = t.curso
-		AND (
-                        (pa.atribuicao = :cod 
-                            AND t.ano = :ano 
-                            AND (t.semestre = 0 OR t.semestre = :semestre)
-                        )
-                    OR (d.numero IN (SELECT d1.numero 
+		AND pa.atribuicao = :cod 
+                AND t.ano = :ano 
+                AND (t.semestre = 0 OR t.semestre = :semestre)
+                GROUP BY pa.semana
+                ORDER BY pa.semana";
+
+        $params = array(':cod'=> $codigo, ':ano'=> $ano, ':semestre'=> $semestre);
+        $res = $bd->selectDB($sql, $params);
+        if (!$res) {
+            $res = $this->getConteudosAulasEquivalente($codigo, $ano, $semestre);
+        }
+        
+        if ( $res )
+        {
+            return $res;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    // USADO POR: PROFESSOR/AULA.PHP
+    // LISTA AS SEMANAS DE AULA DO PROFESSOR
+    public function getConteudosAulasEquivalente($codigo, $ano, $semestre) {
+        print $codigo;
+        $bd = new database();
+
+        $sql = "SELECT pa.conteudo, pa.semana, pe.numeroAulaSemanal 
+		FROM PlanosAula pa, PlanosEnsino pe, Atribuicoes a, 
+                    Disciplinas d, Turmas t, Cursos c
+		WHERE pa.atribuicao = pe.atribuicao 
+		AND pe.atribuicao = a.codigo
+		AND a.disciplina = d.codigo
+                AND t.codigo = a.turma
+                AND c.codigo = t.curso
+		AND d.numero IN (SELECT d1.numero 
                             FROM Disciplinas d1, Atribuicoes a1 
                             WHERE a1.disciplina = d1.codigo 
                             AND d1.numero = d.numero 
                             AND a1.codigo = :cod
                             AND t.ano = :ano)
-                        AND 
-                        t.numero IN (SELECT t1.numero 
+                AND t.numero IN (SELECT t1.numero 
                             FROM Atribuicoes a1, Turmas t1
                             WHERE t1.codigo = a1.turma
                             AND a1.codigo = :cod
                             AND t.ano = :ano)
-                        )
-                    )
                 GROUP BY pa.semana
                 ORDER BY pa.semana";
 
