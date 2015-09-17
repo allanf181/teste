2d5198521da9bd6262d2af6102fa0d6bf7aef757
JOAO PAULO LEMOS ESCOLA <jpescola@ifsp.edu.br>
2015-09-17 09:24:11 -0300

    Correção da URL de recuperação de senha

--- controller/login.class.php
+++ controller/login.class.php
@@ -128,7 +128,7 @@ class login extends Generic {
             // Gerando uma chave para a recuperacao email
             $chave = date("Ymd") . '$' . time() . '$' . 'IFSP';
             $chave = sha1($chave);
-            $chv = 'https://' . $_SERVER['HTTP_HOST'] . '/academico/index.php?key=' . $chave . '&prt=' . base64_encode($prontuario);
+            $chv = 'https://' . $_SERVER['HTTP_HOST'] . '/webdiario/index.php?key=' . $chave . '&prt=' . base64_encode($prontuario);
 
             // Guarda a chave no banco
             $sql = "UPDATE Pessoas SET recuperaSenha = :chave "
