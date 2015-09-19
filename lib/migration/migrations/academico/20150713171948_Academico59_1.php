0589563013db4a93b25383e6b52b88b1d4c5b187
JOAO PAULO LEMOS ESCOLA <jpescola@ifsp.edu.br>
2015-09-19 11:47:59 -0300

    Atualização do BackupOnline

---
+++ lib/migration/migrations/academico/20150713171948_Academico59_1.php
@@ -0,0 +1,17 @@
+<?php
+
+class Academico59 extends Ruckusing_Migration_Base
+{
+    public function up()
+    {
+        // ATUALIZAR VERSAO ATUAL
+        $this->execute("UPDATE Instituicoes SET versao='443', versaoAtual='443'");
+        printf("\nPatch Academico59_1: OK\n\n");
+        
+    }//up()
+
+    public function down()
+    {
+    }//down()
+    
+}
