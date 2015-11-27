f0aee2b4a08449bfe74663c9845726a59e0943c0
JOAO PAULO LEMOS ESCOLA <jpescola@ifsp.edu.br>
2015-11-27 19:28:42 -0200

    Correção na classe de notas para resolver o erro de média devido a atribuicoes lançadas e sem uso na turma.

--- lib/migration/migrations/academico/20151127000065_WebDiario65.php
+++ lib/migration/migrations/academico/20151127000065_WebDiario65.php
@@ -4,10 +4,10 @@ class WebDiario65 extends Ruckusing_Migration_Base
 {
     public function up()
     {
-        $this->execute("alter table PlanosAula drop foreign key PlanosAula_ibfk_1;");
-        $this->execute("alter table PlanosAula add constraint PlanosAula_ibfk_1 foreign key(atribuicao) references Atribuicoes(codigo) on delete cascade;");
-        $this->execute("alter table PlanosEnsino drop foreign key PlanosEnsino_ibfk_1;");
-        $this->execute("alter table PlanosEnsino add constraint PlanosEnsino_ibfk_1 foreign key (atribuicao) references Atribuicoes(codigo) on delete cascade;");
+//        $this->execute("alter table PlanosAula drop foreign key PlanosAula_ibfk_1;");
+//        $this->execute("alter table PlanosAula add constraint PlanosAula_ibfk_1 foreign key(atribuicao) references Atribuicoes(codigo) on delete cascade;");
+//        $this->execute("alter table PlanosEnsino drop foreign key PlanosEnsino_ibfk_1;");
+//        $this->execute("alter table PlanosEnsino add constraint PlanosEnsino_ibfk_1 foreign key (atribuicao) references Atribuicoes(codigo) on delete cascade;");
 
         // ATUALIZAR VERSAO ATUAL
         $this->execute("UPDATE Instituicoes SET versao='447', versaoAtual='447'");
