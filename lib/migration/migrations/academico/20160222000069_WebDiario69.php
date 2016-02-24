066fb12743c982ea284d3fe191fe60b8283efc71
JOAO PAULO LEMOS ESCOLA <jpescola@ifsp.edu.br>
2016-02-24 12:16:24 -0300

    Correção no arquivo de versionamento.

--- lib/migration/migrations/academico/20160222000069_WebDiario69.php
+++ lib/migration/migrations/academico/20160222000069_WebDiario69.php
@@ -4,10 +4,24 @@ class WebDiario69 extends Ruckusing_Migration_Base
 {
     public function up()
     {
-        $this->execute("alter table TDComponente add column prioridade char(1);");
-        $this->execute("alter table TDComponente add column referencia char(1);");
-        $this->execute("alter table TDComponente change periodo turno char(1);");
-        $this->execute("alter table TDAtvECmt add column referencia char(1);");
+        
+        if ($this->execute("SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'TDComponente' AND column_name = 'prioridade'"))
+            print "\nok"; 
+        else
+            $this->execute("alter table TDComponente add column prioridade char(1);");
+        if ($this->execute("SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'TDComponente' AND column_name = 'referencia'"))
+            print "\nok";
+        else
+            $this->execute("alter table TDComponente add column referencia char(1);");
+        if ($this->execute("SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'TDComponente' AND column_name = 'turno'"))
+            print "\nok";
+        else
+            $this->execute("alter table TDComponente change periodo turno char(1);");
+        if ($this->execute("SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'TDAtvECmt' AND column_name = 'referencia'"))
+            print "\nok";
+        else
+            $this->execute("alter table TDAtvECmt add column referencia char(1);");
+
         $this->execute("UPDATE Instituicoes SET versao='449', versaoAtual='449'");
 
         printf("\nPatch WebDiario 69: OK\n\n");
